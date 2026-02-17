67d962930868f68b59acbb28d0d1c2019d1d42e0
minhazchy101 <minhazchowdhury101@gmail.com>
2026-02-17 19:34:03 +0600

    feat: spinner added

--- script.js
+++ script.js
@@ -1,74 +1,32 @@
-const featuresData = [
-  {
-    id: 1,
-    icon: "fa-shipping-fast",
-    title: "Fast Delivery",
-    description:
-      "Get your orders delivered quickly and safely right to your doorstep.",
-  },
-  {
-    id: 2,
-    icon: "fa-headset",
-    title: "24/7 Support",
-    description:
-      "Our support team is available around the clock to assist you anytime.",
-  },
-  {
-    id: 3,
-    icon: "fa-lock",
-    title: "Secure Payment",
-    description:
-      "Your transactions are encrypted and protected with advanced security systems.",
-  },
-  {
-    id: 4,
-    icon: "fa-undo-alt",
-    title: "Easy Returns",
-    description:
-      "Hassle-free returns and exchanges within 30 days of purchase.",
-  },
-];
-
-const displayFeatures = () => {
-  const featuresContainer = document.getElementById("features-container");
-  featuresData.forEach((feature) => {
-    const featureCard = document.createElement("div");
-    featureCard.innerHTML = `
-    <div class="card bg-white card-lg shadow-md relative h-44 items-center">
-  <div class="card-body">
-    <div class="bg-[#1128ab53] w-fit p-2 rounded-md absolute -top-4 border-2 border-white">
-         <i class="fas ${feature.icon}"></i>
-    </div>
-   
-
-    <h2 class="card-title">${feature.title}</h2>
-    <p>${feature.description}</p>
-  </div>
-</div>
-    `;
-    featuresContainer.append(featureCard);
-  });
-};
-
-
 const fetchProducts = () => {
+     mangeSpinner(true)
   const url = "https://fakestoreapi.com/products";
   fetch(url)
     .then((response) => response.json())
-    .then((data) =>{
+    .then((data) => {
       removeActive();
       let allBtn = document.getElementById("catBtn-all");
-      allBtn.classList.add("sass-active")
-      displayProducts(data)});
+      allBtn.classList.add("sass-active");
+      displayProducts(data);
+    });
 };
-let allBtn = document.getElementById("catBtn-all");
-allBtn.addEventListener("click", fetchProducts)
+const mangeSpinner =(status)=>{
+    if(status == true){
+        document.getElementById("spinner").classList.add("flex")
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("products-container").classList.add("hidden")
+    }
+    else{
+         document.getElementById("spinner").classList.add("hidden")
+        document.getElementById("spinner").classList.remove("flex")
+        document.getElementById("products-container").classList.remove("hidden")
+    }
+}
 
-const displayProducts =(products)=>{
-  //  const topTrend = trends.splice(0, 3);
-  // console.log(topTrend);
+
+const displayProducts = (products) => {
   const productsContainer = document.getElementById("products-container");
-   productsContainer.innerHTML = " ";
+  productsContainer.innerHTML = " ";
   products.forEach((product) => {
     const productsCard = document.createElement("div");
     productsCard.innerHTML = `
@@ -106,99 +64,81 @@ const displayProducts =(products)=>{
 </div>
     `;
     productsContainer.append(productsCard);
-  });
-
-}
-
-const displayTrend = (trends) => {
-  const topTrend = trends.splice(0, 3);
-  // console.log(topTrend);
-  const trendContainer = document.getElementById("trend-container");
-  topTrend.forEach((trend) => {
-    const trendCard = document.createElement("div");
-    trendCard.innerHTML = `
-    <div class="card bg-base-100 shadow-sm">
-  <figure>
-    <img
-  src="${trend.image}"
-  alt="Shoes"
-  class="w-64 h-84 object-cover rounded-lg"
-/>
-
-  </figure>
-  
-  <div class="card-body h-60">
-   <div class="badge bg-[#112bbe53] text-sm font-medium">${trend.category}</div>
-    <h2 class="card-title">
-      ${trend.title}
-      
-         <span class="font-semibold flex items-center"><i class="fas fa-star text-yellow-500"></i>
- ${trend.rating.rate}(${trend.rating.count})</span>
-    </h2>
-
-    <p class="text-lg font-bold text-[#0e1438cf]">$ ${trend.price}</p>
-    <div class="card-actions flex justify-between">
-      <div class="flex-1"> <button class="btn btn-outline btn-sm gap-2 w-full">
-        <i class="fa-solid fa-eye"></i> Details
-      </button>
-     
-      </div>
-      <div class="flex-1">  <button class="btn btn-primary btn-sm gap-2 w-full">
-        <i class="fas fa-cart-plus"></i> Add
-      </button></div>
-    </div>
-  </div>
-</div>
-    `;
-    trendContainer.append(trendCard);
+       mangeSpinner(false)
   });
 };
 
-const fetchCategory =()=>{
+const fetchCategory = () => {
   const url = "https://fakestoreapi.com/products/categories";
   fetch(url)
     .then((response) => response.json())
     .then((data) => displayCategory(data));
-}
+};
 
-const displayCategory =(categories)=>{
-      const categoryContainer = document.getElementById("category-container");
-      categories.forEach((category)=>{
-       
-        const categoryCard = document.createElement("div")
-        categoryCard.innerHTML = `
-        <button id="catBtn-${category}" class="sass-btn sass-btn-soft sass-btn-primary" onclick="loadCategory('${category}')">${category.toUpperCase()}</button>
-        `
-        categoryContainer.append(categoryCard);
-      })
-}
+const displayCategory = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  categoryContainer.innerHTML = "";
 
-const removeActive =()=>{
-     const lBtn = document.querySelectorAll(".sass-btn")
-    //  lBtn.forEach(btn => console.log(btn))
-     lBtn.forEach(btn => btn.classList.remove("sass-active"))
-}
-const loadCategory =(category)=>{
-      const url = `https://fakestoreapi.com/products/category/${category}`;
+
+  const allButton = document.createElement("button");
+  allButton.id = "catBtn-all";
+  allButton.className =
+    "sass-btn sass-btn-soft sass-btn-primary sass-active";
+  allButton.textContent = "ALL";
+
+  allButton.addEventListener("click", () => {
+    fetchProducts();
+  });
+
+  categoryContainer.append(allButton);
+
+  categories.forEach((category) => {
+    const safeId = category.replace(/[^a-z0-9]/gi, "");
+
+    const button = document.createElement("button");
+    button.id = `catBtn-${safeId}`;
+    button.className = "sass-btn sass-btn-soft sass-btn-primary";
+    button.textContent = category.toUpperCase();
+
+    button.addEventListener("click", () => {
+      loadCategory(category);
+    });
+
+    categoryContainer.append(button);
+  });
+};
+
+
+const removeActive = () => {
+  const lBtn = document.querySelectorAll(".sass-btn");
+  //  lBtn.forEach(btn => console.log(btn))
+  lBtn.forEach((btn) => btn.classList.remove("sass-active"));
+};
+const loadCategory = (category) => {
+  mangeSpinner(true)
+  const encodedCategory = encodeURIComponent(category);
+  const safeId = category.replace(/[^a-z0-9]/gi, "");
+  const url = `https://fakestoreapi.com/products/category/${encodedCategory}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) =>{
+    .then((data) => {
       removeActive();
-      const cBtn = document.getElementById(`catBtn-${category}`)
-      cBtn.classList.add("sass-active")
-      displayProducts(data)});
-}
+      const cBtn = document.getElementById(`catBtn-${safeId}`);
+      cBtn.classList.add("sass-active");
+      displayProducts(data);
+    });
+};
 
-const fetchDetails =(id)=>{
-      const url = `https://fakestoreapi.com/products/${id}`
-     fetch(url)
+const fetchDetails = (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
     .then((res) => res.json())
     .then((data) => displayModal(data));
-}
+};
 
-const displayModal =(details)=>{
-  console.log(details)
-  const detailsContainer = document.getElementById("details-container")
+const displayModal = (details) => {
+  console.log(details);
+  const detailsContainer = document.getElementById("details-container");
   detailsContainer.innerHTML = `
    <div class="card">
       <figure class="h-56 overflow-hidden">
@@ -240,12 +180,10 @@ const displayModal =(details)=>{
     </div>
       </div>
     </div>
-  `
+  `;
 
   document.getElementById("details_modal").showModal();
-}
+};
 
 fetchCategory();
 fetchProducts();
-// displayFeatures();
-// displayTrend()
